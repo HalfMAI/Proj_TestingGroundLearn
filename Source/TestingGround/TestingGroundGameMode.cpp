242fa0d591ffc2056b0ff32c1beacf6e0b38d87d
HALFMAI-SURFACE\HalfL <halfloligo.h@gmail.com>
2017-12-15 18:00:06 +0800

    rename the PlayerPawn

--- Source/TestingGround/TestingGroundGameMode.cpp
+++ Source/TestingGround/TestingGroundGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundGameMode::ATestingGroundGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/BP_Player"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 	// use our custom HUD class
 	HUDClass = ATestingGroundHUD::StaticClass();
